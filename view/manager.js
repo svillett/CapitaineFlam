dafb8062763e681c37a0094d4fada85cfbabf1e2
Lajule <julien.rouzieres@mac.com>
2012-08-22 20:23:37 +0200

    2nd commit
    always a calendar in page, but plz don't revert me again,
    regards

--- view/manager.js
+++ view/manager.js
@@ -6,6 +6,19 @@ define(function (require, exports, module) {
     'use strict';
 
     function _initView() {
+        var $inputDate = $("#inputDate");
+
+        var now = new Date();
+        var month = now.getMonth() + 1;
+        var today = now.getDate() + '/' + month + '/' + now.getFullYear();
+
+        $inputDate.attr("data-date", today);
+        $inputDate.attr("value", today);
+
+        $inputDate.datepicker({
+            format: "d/m/yyyy"
+        });
+
         $('#gamme a').click(function (e) {
             e.preventDefault();
             $(this).tab('show');
